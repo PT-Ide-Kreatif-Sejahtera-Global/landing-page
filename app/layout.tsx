03b8a32e62a2eb2e6cf278a4df22eb3b2f775a2c
Akbara Jati <82393953+GayuhJati@users.noreply.github.com>
2024-11-10 23:20:37 +0700

    Origin/akbar (#3)
    * feat(home): Add component in landing page
    
    * feat(Navbar): Creating a navbar
    
    * feat(Navbar): Navbar With Menut
    
    * fix(navbar)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,21 +3,18 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Inter } from 'next/font/google';
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({
+  subsets: ['latin'], 
+  variable: '--font-inter',
+  display: "swap",
+  weight: ["400", "700", "200", "300", "500", "600", "100"],
 });
 
 export const metadata: Metadata = {
   title: "IdeaThings",
-  description: "A Place For To Grow Your Talent",
+  description: "A Place To Grow Your Talent",
 };
 
 export default function RootLayout({
@@ -28,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} antialiased`}
       >
         <Navbar/>
         {children}
