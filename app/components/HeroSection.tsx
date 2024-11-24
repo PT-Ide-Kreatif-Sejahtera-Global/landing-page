680a38960d3f9191e2ff66e7caf205dc0cf323dc
Akbara Jati <82393953+GayuhJati@users.noreply.github.com>
2024-11-24 16:36:59 +0700

    feat(hero section) (kalau ada salah hubungi saya)
    * feat(home): Add component in landing page
    
    * feat(Navbar): Creating a navbar
    
    * feat(Navbar): Navbar With Menut
    
    * fix(navbar)
    
    * feat(hero): creating section hero

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -4,22 +4,22 @@ import Link from 'next/link'
 
 const HeroSection = () => {
   return (
-    <>
-      <div className='px-4 md:px-28 py-24'>
-          <div className='text-center gap-10 flex flex-col justify-center items-center'>
-              <h1 className='text-4xl md:text-5xl font-bold'>Grow Your Unique Talent With IdeaThings!</h1>
-              <h2 className='text-xl md:text-2xl font-bold'>Temukan bakat dan kembangkan bisnis mu bersama kami</h2>
-              <p className='md:text-xl'>Bersama IdeaThigs mari wujudkan mimpimu!</p>
-
-              <Link href={'/'} target='_blank'>
-                  <button className='bg-bran py-3 px-5 rounded-xl font-bold animate-bounce hover:scale-105 hover:text-white hover:bg-black duration-300'>register now</button>
-              </Link>
+    <div className='max-h-screen-hpkecil lg:h-full'>
+      <div className='relative z-0 self-stretch w-full'>
+        <div className='inset-x-0 bottom-0 pt-[300px]'>
+          <img src="/hero/wave.png" alt="wave-background" className='top-10' />
+        </div>
+        <div className='absolute inset-0 z-10 px-20 py-10'>
+          <div>
+            <div>Grow your unique
+            talent with us !</div>
+            <div>Improve your skill to build your personal branding.</div>
+            <button className='px-[20px] py-[10px] bg-[#ACF826] text-[16px] font-bold rounded-xl'>Join Now</button>
           </div>
+          <div>gambar</div>
+        </div>
       </div>
-      <div className='bottom-0 '>
-          <Image className='w-full h-auto' src={'/floor.svg'} width={200} height={200} alt={''}></Image>
-      </div>
-    </>
+    </div>
   )
 }
 
