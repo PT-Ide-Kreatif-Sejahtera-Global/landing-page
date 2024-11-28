b8ee44378ee4c83ebde41bf143b779d2d4ee3a03
IvanSahmura <ivansahmura123@gmail.com>
2024-11-28 15:50:40 +0700

    progress education

--- app/components/EducationSection.tsx
+++ app/components/EducationSection.tsx
@@ -17,15 +17,13 @@ const IMGITEM = [
   {
     img:'/Education_Assets/card-1.png'
   },
-  {
-    img:'/Education_Assets/card-1.png'
-  }
+  
 ]
 
 const EducationSection = () => {
 
   return (
-    <div className='px-4 md:px-28 mt-32'>
+    <div className='px-4 md:px-40'>
         <div className=' flex flex-col md:flex-col gap-5 lg:flex-row items-center justify-between '>
             <div className='flex flex-col text-center gap-3  lg:text-start'>
                 <h1 className='text-xl lg:text-2xl font-bold '>
@@ -36,21 +34,29 @@ const EducationSection = () => {
                 </p>
             </div>
             <div>
-                <Link href={'/'} target='_blank'>
+                <a href="http://">
                   <button className=' flex gap-3 border-black border-2 border-solid px-4 py-1 rounded-md items-center font-bold hover:text-lg duration-300'>
                     More
                   <PiArrowUpRightBold />
                   </button>
-                </Link>
+                </a>
             </div>
         </div>
 
-        <div className='grid md:grid-cols-2 lg:grid-cols-4 justify-center items-center gap-20 mt-10'>
+        <div className='flex flex-col  lg:flex-row justify-center items-center gap-20 mt-10'>
             {IMGITEM.map((item, i) => (
               <div key={i} className='' >
-                  <div className='w-64 h-80 relative  overflow-hidden rounded-lg hover:scale-105 duration-300 group cursor-pointer  object-cover '>
+                  {/* <div className='w-64 h-80 relative  overflow-hidden rounded-lg hover:scale-105 duration-300 group cursor-pointer  object-cover '>
                       <Image className='rounded-xl border-4 border-black opacity-40 group-hover:opacity-100 duration-300 '  src={item.img} alt="Logo" fill      />
                       <div className='absolute bottom-0 w-full bg-gradient-to-t  from-black to-transparent px-3 translate-y-full py-4 group-hover:translate-y-0 duration-300'>
+                          <p className=' text-white font-bold text-xl'>{item.title}</p>
+                          <p className='text-sm text-white'>{item.subtitle}</p>
+                      </div>
+                  </div> */}
+
+                  <div className='w-64 h-80 relative overflow-hidden  object-cover '>
+                      <img className=''  src={item.img} alt="Logo"       />
+                      <div className='absolute bottom-0 w-full bg-red-500 h-36'>
                           <p className=' text-white font-bold text-xl'>{item.title}</p>
                           <p className='text-sm text-white'>{item.subtitle}</p>
                       </div>
