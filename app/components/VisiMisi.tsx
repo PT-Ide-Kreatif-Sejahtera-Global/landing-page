ae4e85d946637128518d5e5f4deb1648623c4ccb
Akbara Jati <82393953+GayuhJati@users.noreply.github.com>
2024-11-28 22:23:19 +0700

    add section about and misi (#13)
    * feat(home): Add component in landing page
    
    * feat(Navbar): Creating a navbar
    
    * feat(Navbar): Navbar With Menut
    
    * fix(navbar)
    
    * feat(hero): creating section hero
    
    * Fix hero
    
    * feat(home): Add visi misi and about company section and fix navbar

---
+++ app/components/VisiMisi.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const VisiMisi = () => {
+  return (
+    <div className="container max-w-screen-2xl mx-auto px-20 flex flex-row ">
+      <div className="basis-1/2 flex flex-col text-[18px] bg-slate-50 p-12">
+        <div>
+          <span className="font-bold">VISI</span> <br />
+          <hr />
+          <p>
+            Menjadikan iDeaThings sebagai perusahaan sociopreneur yang
+            menciptakan lingkungan inklusif bagi seluruh lapisan masyarakat.
+            Kami berkomitmen untuk menyediakan peluang kerja bagi kaum difabel
+            dan menjawab tantangan yang dihadapi oleh kaum marjinal. Melalui
+            visi ini, kami berharap dapat membuka berbagai kesempatan emas untuk
+            mewujudkan masyarakat yang lebih inklusif dan berdaya.
+          </p>{" "}
+          <br />
+          <span className="font-bold">MISI</span> <br />
+          <hr />
+          <p>
+            iDeaThings menghadirkan peluang kerja bagi kaum marjinal, terutama
+            mereka yang kurang memiliki akses pendidikan, sebagai langkah nyata
+            untuk menciptakan perubahan sosial. Kami bertekad untuk memperluas
+            dampak usaha ini dengan mengembangkan inovasi di berbagai bidang,
+            seperti pengembangan aplikasi, EduTech, E-Commerce, dan sektor
+            lainnya, demi membangun ekosistem yang inklusif dan berkelanjutan.
+          </p>
+        </div>
+        <div></div>
+      </div>
+      <div className="basis-1/2 text-7xl flex items-center bg-[#e6fa9c] p-12 font-semibold">
+        Vision and Mision
+      </div>
+    </div>
+  );
+};
+
+export default VisiMisi;
