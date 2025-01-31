ffaba35e2c9e7fc1080fa429063879d278f3afd8
Faisal Sabillah <faisalsabillah2@gmail.com>
2025-01-31 22:30:07 +0700

    Fix: Fix Bug Homepage Landing Page
     Fix Bug Homepage Landing Page

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -9,24 +9,22 @@ const HeroSection = () => {
   };
   return (
     <div className="max-h-full md:h-full mt-10 xl:mt-0">
-      <div className="relative z-0 self-stretch w-full">
+      <div className="relative z-0 w-full h-full">
         <div className="inset-x-0 bottom-0 pt-[525px] xl:pt-[340px]">
-          <img src="/hero/wave.png" alt="wave-background" className="top-10" />
+          <img src="/hero/wave.png" alt="wave-background" className="w-full h-auto object-cover" />
         </div>
-        <div className="absolute inset-0 z-10 px-5 xl:px-20 py-10">
-          <div className="flex flex-col xl:flex-row gap-5 justify-between">
-            <div className="flex flex-col justify-center items-center xl:items-start gap-6 xl:gap-4">
-              <div className="text-5xl text-center xl:text-start xl:text-6xl font-bold xl:w-[500px]">
-                Grow your unique talent with us !
-              </div>
-              <div className="text-center xl:text-start ">Improve your skill to build your personal branding.</div>
-              <button onClick={handleNavigation} className="py-[10px] bg-lime-400 hover:bg-[#9cdd2c] text-[20px] font-bold rounded-xl w-[150px] ">
-                Join Now
-              </button>
+        <div className="absolute inset-0 flex flex-col xl:flex-row"> {/* Meningkatkan z-index untuk teks */}
+          <div className="flex flex-col justify-center items-center xl:items-start gap-6 xl:gap-4 w-full xl:w-[500px] px-5 xl:px-20 py-10 z-20">
+            <div className="text-5xl text-center xl:text-start xl:text-6xl font-bold">
+              Grow your unique talent with us!
             </div>
-            <div>
-              <img src="/hero/hero1.png" alt="band" className="xl:h-[500px]" />
-            </div>
+            <div className="text-center xl:text-start">Improve your skill to build your personal branding.</div>
+            <button onClick={handleNavigation} className="py-[10px] bg-lime-400 hover:bg-[#9cdd2c] text-[20px] font-bold rounded-xl w-[150px]">
+              Join Now
+            </button>
+          </div>
+          <div className="absolute right-0 top-0 h-full w-auto z-10"> {/* Gambar di belakang teks */}
+            <img src="/hero/hero1.png" alt="band" className="h-full w-auto object-cover" />
           </div>
         </div>
       </div>
