729a42d27e34fd2c1915f0936e2042531868845f
zidane.nur004 <zidane.nur004@gmail.com>
2025-02-05 09:24:17 +0000

    navbar ui improvement

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -50,7 +50,7 @@ const Navbar = () => {
   return (
     <>
       <div className={`fixed top-0 left-0 h-screen w-screen bg-black/40 z-10 ${ isOpen ? "block" : "hidden" }`}></div>
-      <nav className="flex justify-between items-center px-8 py-4 z-[100] bg-white/40 shadow-xl">
+      <nav className="flex justify-between items-center px-8 py-4 z-[100] bg-white shadow-xl">
         <Link href="/" className="flex items-center cursor-pointer">
           <img
             width={50}
@@ -103,16 +103,16 @@ const Navbar = () => {
               alt="logo"
               width={60}
               height={60}
-              className="mb-4 rounded-full"
+              className="mb-2 rounded-full"
             />
           </Link>
           {menuItems.map((item, index) => (
-            <div key={index} className="py-2">
+            <div key={index} className="gap-y-3 flex flex-col w-full items-center">
               <button
                 onClick={() => {
                   handleMenuClick(item.href);
                 }}
-                className="block text-left w-full text-[#2e2751] text-lg font-medium"
+                className="text-center text-white bg-lime-600 w-fit px-4 text-lg font-bold rounded-full hover:bg-lime-400 duration-300"
               >
                 {item.name}
               </button>
